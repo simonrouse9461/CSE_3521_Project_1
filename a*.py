a8bb01ddc394361cb80580aa9daabac3cd7e2f83
Chuhan Feng <simonrouse9461@gmail.com>
2016-02-19 19:02:11 -0500

    update

--- a*.py
+++ a*.py
@@ -2,25 +2,7 @@ from puzzle import *
 from search_agent import SearchAgent
 
 
-def generate_test_problem(problem):
-    if problem == 'shuffle':
-        state = Puzzle(0, 1, 2, 3, 4, 5, 6, 7, 8)
-        state.shuffle(100)
-        return state
-    elif problem == 'easy':
-        state = Puzzle(0, 1, 2, 3, 4, 5, 6, 7, 8)
-        state.shuffle(15)
-        return state
-    elif problem == 'random':
-        return Puzzle.random_puzzle()
-    elif problem == 'project':
-        return Puzzle(5, 0, 4, 2, 1, 3, 6, 7, 8)
-    elif problem == 'unsolvable':
-        return Puzzle(0, 2, 1, 3, 4, 5, 6, 7, 8)
-    else:
-        return Puzzle(0, 1, 2, 3, 4, 5, 6, 7, 8)
-
-init = generate_test_problem('shuffle')
+init = Puzzle(5, 0, 4, 2, 1, 3, 6, 7, 8)
 prob = PuzzleProblem(initial_state=init)
 
 print()
